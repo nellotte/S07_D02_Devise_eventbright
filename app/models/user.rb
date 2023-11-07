06ef5b7e9640edaeaa3c112dd232724123fb1353
nellotte <nelly.guerin@gmail.com>
2023-11-07 11:17:03 +0100

    ajout user mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :attendances
+  has_many :events, through: :attendances
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['8a1b9305ccad38f6ab4c7bbc76c8e9d2'],
+  :password => ENV['6ccc697979f45c5e94bf1b988db41a86'],
+  :domain => 'monsite.fr',
+  :address => 'in-v3.mailjet.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
